67e86deee414b8f8206cf4430deb6717b0eda941
Vicodiru <marve.vicodiru@gmail.com>
2023-07-18 11:39:24 +0100

    completed

---
+++ src/Categories.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Categories = ({ categories, filterItems }) => {
+  return (
+    <div className="btn-container">
+      {categories.map((category) => {
+        console.log(category);
+        return (
+          <button
+            type="button"
+            className="btn"
+            key={category}
+            onClick={() => filterItems(category)}
+          >
+            {category}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Categories;
